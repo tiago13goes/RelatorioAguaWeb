fb94bf45176c002bd88b200c504a91326a229fcd
tiago13goes <tiago.a.s.goes@gmail.com>
2021-11-19 20:37:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# FiapFilmes3ECA
+# Link para a pagina: https://joaosobhie.github.io/FiapFilmes3ECA/
+# Link para o figma: https://www.figma.com/file/8OawkgrmWVFtrXjp8opUzp/Untitled?node-id=102%3A3
+Turma: 3ECA
+Grupo:
+Gustavo Borri de Azevedo        RM: 82226
+João Vitor Ovalle Sobhie        RM: 82361
+Léo Kioshi Onoda            RM: 83479
+Tiago Augusto Santos Góes    RM: 82841
+
---
+++ docs/javascript/index.js
@@ -0,0 +1,241 @@
+var cidades = {
+    "cidades": [{
+            "nomeCidade": "Alto Tietê",
+            "resultado": {
+                "meses": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+                "turbidezR": [662, 664, 661, 656, 649, 682, 681, 679, 677, 667, 666, 676],
+                "turbidezC": [662, 662, 661, 656, 649, 682, 681, 679, 677, 667, 666, 676],
+                "corR": [204, 202, 201, 233, 230, 207, 240, 222, 212, 206, 208, 212],
+                "corC": [204, 200, 201, 233, 229, 207, 238, 220, 212, 204, 204, 212],
+                "cloroR": [662, 674, 661, 656, 650, 682, 678, 679, 674, 664, 663, 676],
+                "cloroC": [662, 674, 661, 656, 642, 682, 678, 679, 674, 664, 663, 676],
+                "coliformeR": [662, 664, 660, 656, 650, 682, 681, 679, 677, 667, 666, 676],
+                "coliformeC": [657, 655, 657, 654, 634, 679, 681, 679, 677, 664, 664, 674],
+                "coliformeE": [662, 664, 660, 656, 650, 682, 681, 679, 677, 667, 666, 676]
+            }
+        },
+
+        {
+            "nomeCidade": "Cantareira",
+            "resultado": {
+                "meses": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+                "turbidezR": [707, 706, 707, 707, 753, 712, 702, 713, 725, 704, 706, 716],
+                "turbidezC": [705, 703, 703, 707, 750, 708, 701, 712, 724, 704, 706, 715],
+                "corR": [240, 237, 238, 295, 318, 322, 268, 253, 262, 247, 250, 280],
+                "corC": [240, 237, 234, 295, 314, 321, 267, 253, 262, 247, 249, 280],
+                "cloroR": [707, 706, 707, 708, 753, 712, 703, 713, 724, 703, 707, 715],
+                "cloroC": [706, 706, 707, 708, 750, 712, 703, 713, 724, 703, 707, 715],
+                "coliformeR": [706, 706, 706, 707, 752, 711, 701, 712, 725, 704, 707, 716],
+                "coliformeC": [703, 698, 692, 703, 746, 707, 698, 711, 722, 702, 706, 708],
+                "coliformeE": [706, 705, 706, 707, 752, 711, 701, 712, 725, 704, 707, 716]
+            }
+        }, {
+            "nomeCidade": "Guarapiranga",
+            "resultado": {
+                "meses": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+                "turbidezR": [800, 806, 799, 802, 800, 819, 799, 797, 800, 803, 811, 805],
+                "turbidezC": [795, 805, 796, 800, 788, 815, 795, 792, 792, 801, 808, 803],
+                "corR": [189, 202, 201, 204, 199, 215, 208, 234, 211, 214, 205, 215],
+                "corC": [184, 202, 200, 204, 196, 215, 207, 233, 208, 214, 204, 214],
+                "cloroR": [800, 805, 799, 801, 800, 819, 798, 799, 799, 805, 808, 805],
+                "cloroC": [799, 801, 796, 799, 790, 814, 795, 792, 797, 795, 785, 784],
+                "coliformeR": [800, 805, 799, 802, 800, 819, 799, 798, 800, 803, 811, 805],
+                "coliformeC": [792, 794, 794, 791, 793, 800, 791, 784, 792, 787, 799, 787],
+                "coliformeE": [800, 803, 799, 801, 800, 819, 798, 798, 800, 803, 809, 805]
+            }
+        },
+
+        {
+            "nomeCidade": "Marsilac",
+            "resultado": {
+                "meses": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+                "turbidezR": [11, 9, 10, 10, 10, 11, 10, 11, 10, 10, 10, 10],
+                "turbidezC": [11, 9, 10, 10, 10, 11, 10, 11, 10, 10, 10, 10],
+                "corR": [6, 5, 5, 8, 4, 6, 6, 7, 8, 6, 7, 7],
+                "corC": [6, 5, 5, 8, 4, 6, 6, 7, 8, 6, 6, 7],
+                "cloroR": [11, 9, 10, 10, 10, 11, 10, 11, 10, 10, 10, 10],
+                "cloroC": [11, 9, 10, 10, 10, 11, 10, 11, 10, 10, 10, 10],
+                "coliformeR": [11, 9, 10, 10, 10, 11, 10, 11, 10, 10, 10, 10],
+                "coliformeC": [11, 9, 10, 10, 10, 11, 10, 10, 10, 10, 10, 10],
+                "coliformeE": [11, 9, 10, 10, 10, 11, 10, 11, 10, 10, 10, 10]
+            }
+        },
+
+        {
+            "nomeCidade": "Poço Oriental",
+            "resultado": {
+                "meses": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+                "turbidezR": [11, 10, 10, 10, 10, 11, 10, 11, 10, 11, 10, 10],
+                "turbidezC": [9, 10, 10, 10, 10, 11, 10, 11, 10, 11, 10, 10],
+                "corR": [4, 5, 4, 8, 4, 6, 6, 8, 8, 7, 7, 7],
+                "corC": [4, 5, 4, 8, 4, 6, 6, 8, 8, 7, 7, 7],
+                "cloroR": [11, 10, 10, 10, 10, 11, 10, 11, 10, 11, 10, 10],
+                "cloroC": [11, 10, 10, 10, 10, 11, 10, 11, 10, 11, 10, 10],
+                "coliformeR": [11, 10, 10, 10, 10, 11, 10, 11, 10, 11, 10, 10],
+                "coliformeC": [11, 10, 10, 10, 10, 11, 10, 11, 10, 11, 10, 10],
+                "coliformeE": [11, 10, 10, 10, 10, 11, 10, 11, 10, 11, 10, 10]
+            }
+        },
+
+        {
+            "nomeCidade": "Rio Claro",
+            "resultado": {
+                "meses": ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+                "turbidezR": [162, 160, 158, 160, 160, 162, 220, 226, 241, 235, 235, 229],
+                "turbidezC": [162, 160, 158, 160, 160, 162, 220, 226, 241, 235, 235, 229],
+                "corR": [43, 44, 44, 64, 49, 67, 74, 70, 78, 70, 66, 65],
+                "corC": [43, 44, 44, 64, 49, 67, 74, 70, 78, 70, 66, 65],
+                "cloroR": [162, 160, 158, 160, 160, 162, 220, 226, 241, 235, 235, 229],
+                "cloroC": [162, 160, 158, 160, 160, 162, 220, 226, 241, 235, 235, 229],
+                "coliformeR": [162, 159, 158, 159, 160, 162, 220, 226, 241, 235, 235, 228],
+                "coliformeC": [162, 157, 153, 158, 158, 157, 219, 225, 241, 230, 235, 228],
+                "coliformeE": [162, 159, 158, 159, 160, 162, 220, 226, 241, 235, 235, 228]
+            }
+        }
+
+
+    ]
+}
+
+
+
+
+
+var onChangeCidade = () => {
+
+    $("#slcCidade").on("change", () => {
+
+        var nomeCidade = $("#slcCidade").val()
+        var tipoResultado = $("#slcTipoResultado").val()
+
+
+        cidades.cidades.forEach(city => {
+            if (city.nomeCidade == nomeCidade) {
+                carregarGrafico(city, tipoResultado)
+                return
+            }
+        })
+        debugger
+
+    })
+}
+
+var onChangeTipoResultado = () => {
+
+    $("#slcTipoResultado").on("change", () => {
+
+        var tipoResultado = $("#slcTipoResultado").val()
+        var nomeCidade = $("#slcCidade").val()
+
+
+        cidades.cidades.forEach(city => {
+            if (city.nomeCidade == nomeCidade) {
+                carregarGrafico(city, tipoResultado)
+                return
+            }
+        })
+        debugger
+
+    })
+}
+
+
+var carregarGrafico = (cidade, tipo) => {
+
+    var labels = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+
+    var realizadoLabel = ""
+    var conformeLabel = ""
+    var titulo = ""
+    var dataRealizado = null
+    var dataConforme = null
+
+
+    switch (tipo) {
+
+        case "turbidez":
+            realizadoLabel = "Turbidez Realizado"
+            conformeLabel = "Turbidez Conforme"
+            dataRealizado = cidade.resultado.turbidezR
+            dataConforme = cidade.resultado.turbidezC
+            titulo = cidade.nomeCidade + " - Turbidez"
+            break;
+
+        case "cor":
+            realizadoLabel = "Cor Realizado"
+            conformeLabel = "Cor Conforme"
+            dataRealizado = cidade.resultado.corR
+            dataConforme = cidade.resultado.corC
+            titulo = cidade.nomeCidade + " - Cor"
+            break;
+
+        case "cloro":
+            realizadoLabel = "Cloro Realizado"
+            conformeLabel = "Cloro Conforme"
+            dataRealizado = cidade.resultado.cloroR
+            dataConforme = cidade.resultado.cloroC
+            titulo = cidade.nomeCidade + "- Cloro"
+            break;
+
+        case "coliforme":
+            realizadoLabel = "Coliforme Realizado"
+            conformeLabel = "Coliforme Conforme"
+            dataRealizado = cidade.resultado.coliformeR
+            dataConforme = cidade.resultado.coliformeC
+            titulo = cidade.nomeCidade + " - Coliforme "
+            break;
+
+        default:
+            break;
+    }
+
+
+    var data = {
+        labels: labels,
+        datasets: [{
+                label: realizadoLabel,
+                backgroundColor: '#1d8dd9',
+                borderColor: '#1d8dd9',
+                data: dataRealizado,
+            },
+            {
+                label: conformeLabel,
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: dataConforme,
+            }
+        ]
+    };
+
+    var config = {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: titulo,
+                }
+            }
+        }
+    };
+
+    let chartStatus = Chart.getChart("myChart");
+
+    if (chartStatus != undefined) {
+        chartStatus.destroy();
+    }
+
+    new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+
+}
+
+var init = () => {
+    onChangeCidade()
+    onChangeTipoResultado()
+    carregarGrafico(cidades.cidades[0], "turbidez")
+}
